152e8907d35e509efee27b71a58c80c05c47bd17
Marius <marius.budreika09@gmail.com>
2022-07-05 17:20:33 +0300

    added new action types to reducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,23 +4,34 @@ import styles from "./counter.module.scss";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: "increment" });
+  };
+
+  const increasetHandler = () => {
+    dispatch({ type: "increase", amount: 10 });
   };
 
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
 
+  const toggleHandler = () => {
+    dispatch({ type: "toggle" });
+  };
+
   return (
     <main className={styles.container}>
       <h1>Redux counter</h1>
-      <div>{counter}</div>
+      {show && <div>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>increment</button>
+        <button onClick={increasetHandler}>increment by 10</button>
         <button onClick={decrementHandler}>decrement</button>
       </div>
+      <button onClick={toggleHandler}>Toggle counter</button>
     </main>
   );
 };
